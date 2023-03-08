fc5964c5360997d9c3423685adca54df85d8e4f6
Joe Wolfe <JoeWolfe0527@gmail.com>
2023-03-07 21:31:46 -0500

    creating exercise for rest and spread operators

---
+++ app.js
@@ -0,0 +1,60 @@
+/*
+Refactor to use the rest operator & an arrow function:
+
+function filterOutOdds() {
+  var nums = Array.prototype.slice.call(arguments);
+  return nums.filter(function(num) {
+    return num % 2 === 0
+  });
+}*/
+function filterOutOdds(...nums) {
+  }
+
+
+
+//Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
+function findMin(...nums){
+}
+
+//Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
+function mergeObjects(obj1, obj2){
+}
+
+//Write a function called ***doubleAndReturnArgs*** which accepts an array and a variable number of arguments. 
+//The function should return a new array with the original array values and all of additional arguments doubled.
+function doubleAndReturnArgs(arr, ...nums){
+}
+
+/** remove a random element in the items array
+and return a new array without that item. */
+
+function removeRandom(items) {
+
+}
+
+/** Return a new array with every item in array1 and array2. */
+
+function extend(array1, array2) {
+}
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+
+function addKeyVal(obj, key, val) {
+}
+
+
+/** Return a new object with a key removed. */
+function removeKey(obj, key) {
+}
+
+
+/** Combine two objects and return a new object. */
+function combine(obj1, obj2) {
+} 
+
+
+
+/** Return a new object with a modified key and value. */
+function update(obj, key, val) {
+}
