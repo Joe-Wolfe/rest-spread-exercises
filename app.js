f30fc1a96dd3deb0cbdc1ff82f708022a29fccfc
Joe Wolfe <JoeWolfe0527@gmail.com>
2023-03-07 21:35:45 -0500

    submitting exercise

--- app.js
+++ app.js
@@ -8,53 +8,66 @@ function filterOutOdds() {
   });
 }*/
 function filterOutOdds(...nums) {
+    return nums.filter(num => num%2 ===0);
   }
 
 
 
 //Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
 function findMin(...nums){
+    return nums.reduce((minValue, val) => Math.min(minValue, val)) 
 }
 
 //Write a function called mergeObjects that accepts two objects and returns a new object which contains all the keys and values of the first object and second object.
 function mergeObjects(obj1, obj2){
+    return {...obj1, ...obj2}
 }
 
 //Write a function called ***doubleAndReturnArgs*** which accepts an array and a variable number of arguments. 
 //The function should return a new array with the original array values and all of additional arguments doubled.
 function doubleAndReturnArgs(arr, ...nums){
+    return [...arr, ...nums.map(num =>num*2)]
 }
 
 /** remove a random element in the items array
 and return a new array without that item. */
 
 function removeRandom(items) {
+    let randomIndex = Math.floor(Math.random() * items.length);
+    return [...items.slice(0, randomIndex), ...items.slice(randomIndex+1)]
 
 }
 
 /** Return a new array with every item in array1 and array2. */
 
 function extend(array1, array2) {
+    return [...array1, ...array2]
 }
 
 /** Return a new object with all the keys and values
 from obj and a new key/value pair */
 
 function addKeyVal(obj, key, val) {
+    return {...obj, [key]: val}
 }
 
 
 /** Return a new object with a key removed. */
 function removeKey(obj, key) {
+   let newObj = {...obj};
+   delete newObj[key]
+   return newObj
 }
 
 
 /** Combine two objects and return a new object. */
 function combine(obj1, obj2) {
+    mergeObjects(obj1, obj2);
 } 
 
 
 
 /** Return a new object with a modified key and value. */
 function update(obj, key, val) {
+    return {...obj, [key]: val}
 }
